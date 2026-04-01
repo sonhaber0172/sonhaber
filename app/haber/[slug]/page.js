9d148b4b1a58a6bebc25c443d7ad2b791c82ebd4
soner0172 <bkacuk2@gmail.com>
2026-04-01 15:13:54 +0300

    haber detay dynamic guncellendi

--- app/haber/[slug]/page.js
+++ app/haber/[slug]/page.js
@@ -1,6 +1,8 @@
 import { supabase } from '../../../lib/supabase'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default async function HaberDetay({ params }) {
   const slug = decodeURIComponent(params.slug)
   
