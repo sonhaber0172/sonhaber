3defedece8377d8c0af95a2f9e979505559b96cf
soner0172 <bkacuk2@gmail.com>
2026-04-09 01:35:07 +0300

    Slider konumu duzeltildi

--- app/page.js
+++ app/page.js
@@ -265,7 +265,11 @@ export default async function HomePage({ searchParams }) {
           </div>
         </div>
       </div>
-{sayfaNo === 1 && <HaberSlider haberler={allNews} />}
+{sayfaNo === 1 && (
+        <div style={{margin: '0', padding: '0'}}>
+          <HaberSlider haberler={allNews} />
+        </div>
+      )}
       {/* ANA İÇERİK */}
       <div className="px-6 py-6 bg-white">
 
